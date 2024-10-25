cc805e34d8057a9c105252d43f6ed2c303ec51a4
Shuwen Wang <shuwen.wang@talentneuron.com>
2024-10-25 15:16:30 -0400

    add cors

--- api/app.py
+++ api/app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+CORS(app)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
-
+    data = {
+        "message": "Hello, World!",
+        "status": "success"
+    }
+    return jsonify(data)
 
 @app.route("/about")
 def about():
