2fc7ff22942b6568b5053f727aff433a5330ff21
Shuwen Wang <shuwen.wang@talentneuron.com>
2024-11-04 10:22:54 -0500

    change ip address to 0.0.0.0

--- api/app.py
+++ api/app.py
@@ -297,7 +297,7 @@ def search_text(task_id):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('--host', default='0.0.0.0')
     parser.add_argument('--port', type=int, default=5009)
     args = parser.parse_args()
 
